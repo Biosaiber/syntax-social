f2ea1918798cb855ceaf9baa4bf98001633db676
isubfox <samuel.liska@gmail.com>
2026-09-12 01:50:30 +0200

    feat: implement admin mode service

--- src/app/services/admin.service.ts
+++ src/app/services/admin.service.ts
@@ -1,8 +1,18 @@
-import { Injectable } from '@angular/core';
+import { Injectable, WritableSignal, signal } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AdminService {
-  
+  private isAdminMode: WritableSignal<boolean> = signal(false);
+
+  getAdminMode() {
+    return this.isAdminMode.asReadonly();
+  }
+
+  toggleAdminMode(): void {
+    this.isAdminMode.update(adminMode => {
+      return !adminMode;
+    })
+  }
 }
