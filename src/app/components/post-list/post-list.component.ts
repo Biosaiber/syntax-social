cecb56d9762f786a5c9dfc5f22d7b38c6ee1d21d
isubfox <samuel.liska@gmail.com>
2026-09-12 13:50:23 +0200

    feat: render one PostComponent for each pos

--- src/app/components/post-list/post-list.component.ts
+++ src/app/components/post-list/post-list.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { DataService } from '../../services/data.service';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-post-list',
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './post-list.component.html',
   styleUrl: './post-list.component.css',
 })
 export class PostListComponent {
+  private dataService = inject(DataService);
 
+  posts = this.dataService.getPostsFromStore()();
 }
+
+// read the list of posts from DataService done
+// render one PostComponent for each post done
+// pass postId to each PostComponent ?
