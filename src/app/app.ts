d3c3cd61aa0edea1cc7b6d610cd3dcc2221af637
isubfox <samuel.liska@gmail.com>
2026-09-12 11:49:32 +0200

    feat: delete routing outlet from app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class AppComponent {
-  protected readonly title = signal('syntax-social');
 }
 
 /* Responsibilities:
