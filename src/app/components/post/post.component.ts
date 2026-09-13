259af70ca6e103ad7601d36479d92f91c5466dd4
isubfox <samuel.liska@gmail.com>
2026-09-13 13:40:02 +0200

    feat input postId and computing posts

--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, input, inject, computed } from '@angular/core';
+import { DataService } from '../../services/data.service';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-post',
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './post.component.html',
   styleUrl: './post.component.css',
 })
 export class PostComponent {
-
+  private dataService = inject(DataService);
+  postId = input.required<number>();
+  post = computed(() => {
+    return this.dataService.getPost(this.postId())();
+  })
 }
