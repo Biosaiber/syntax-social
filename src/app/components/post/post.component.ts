44a59bc6abe39985e246cd0aba7a9edd3b29041e
isubfox <samuel.liska@gmail.com>
2026-09-15 21:02:19 +0200

    feat: add admin caption editing with autosave

--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -1,6 +1,8 @@
 import { Component, input, inject, computed } from '@angular/core';
 import { DataService } from '../../services/data.service';
 import { NgOptimizedImage } from '@angular/common';
+import { AdminService } from '../../services/admin.service';
+import { Subject, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-post',
@@ -10,6 +12,9 @@ import { NgOptimizedImage } from '@angular/common';
 })
 export class PostComponent {
   private dataService = inject(DataService);
+  private adminService = inject(AdminService);
+  isAdminOn = this.adminService.getAdminMode();
+  captionChanges = new Subject<string>();
   postId = input.required<number>();
   post = computed(() => {
     return this.dataService.getPost(this.postId())();
@@ -31,18 +36,31 @@ export class PostComponent {
     return this.dataService.getUser(currentPost.authorId)();
   })
 
-  toggleLike():void {
+  ngOnInit(): void {
+    this.captionChanges.pipe(debounceTime(500)).subscribe(text => {
+      this.dataService.updateCaption(this.postId(), text);
+      console.log(this.post());
+    })
+  }
+
+  toggleLike(): void {
     this.dataService.toggleLike(this.postId());
   }
-  toggleRepost():void {
+  toggleRepost(): void {
     this.dataService.toggleRepost(this.postId());
   }
-  toggleFollow():void {
+  toggleFollow(): void {
     const currentAuthorId = this.author()?.id;
     if (!currentAuthorId) {
       return;
     }
     this.dataService.toggleFollow(currentAuthorId);
+  }
+  captionChangeEvent(event: Event): void {
+    if (event.target instanceof HTMLInputElement) {
+      const text = event.target.value;
+      this.captionChanges.next(text);
+    }
   }
 
 }
@@ -57,6 +75,6 @@ export class PostComponent {
 - trigger like / unlike actions : DONE
 - trigger repost / unrepost actions : DONE
 - trigger follow / unfollow actions : DONE
-- react to admin mode
-- show an editable caption in admin mode
-- trigger the caption autosave flow */
+- react to admin mode : DONE
+- show an editable caption in admin mode : DONE
+- trigger the caption autosave flow : DONE*/
